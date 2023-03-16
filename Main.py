a46c9ad3066f5f14c94329106ba01d2db484e904
Gastaccount CCS <cernadavid1@gmail.com!>
2023-03-16 12:08:05 +0100

    slight update

--- Main.py
+++ Main.py
@@ -9,14 +9,16 @@ from Test import *
 
 
 if __name__ == "__main__":
-    #Test.test()
     assert(len(argv)>=2)
-    debug = int(argv[3]) if len(argv)==4 and argv[2] == "debug" else 0
-    assert(4>=debug and debug>=0)
-    tp =TermParser()
-    I = Interpretation()
-    with open(argv[1]) as f:
-        unif, mappings= tp.parse_input(f.readlines())
-        for m in mappings:I.add_mapping(*m)
-        lu = LoopUnif(I,debug,unif)
-        lu.loop_unif()
+    if argv[1] =="Test":
+        Test.test()
+    else:
+        debug = int(argv[3]) if len(argv)==4 and argv[2] == "debug" else 0
+        assert(4>=debug and debug>=0)
+        tp =TermParser()
+        I = Interpretation()
+        with open(argv[1]) as f:
+            unif, mappings= tp.parse_input(f.readlines())
+            for m in mappings:I.add_mapping(*m)
+            lu = LoopUnif(I,debug,unif)
+            lu.loop_unif()
