b1a4cc10758108ce6d6992f35103f1398b0955d8
Gastaccount CCS <cernadavid1@gmail.com!>
2023-06-18 17:45:22 +0200

    Better commandline parsing

--- Main.py
+++ Main.py
@@ -5,24 +5,28 @@ from functools import reduce
 from TermParser import *
 from os import listdir
 from Test import *
+import argparse
+CodeDescription='Algorithms for deciding unifiability of simple loop unification problem.'
 
-
-
+def parsing_CMD():
+        parser = argparse.ArgumentParser(description=CodeDescription)
+        parser.add_argument('procedure', choices=['Test','Build','Amm'], help='Currently either Test, Build, or Augmented Martelli-Montanari.')
+        parser.add_argument('unif_prob',metavar="file.su", default="",help='The unification problem to Solve. Igored when in Test mode')
+        parser.add_argument('--debug',metavar="int",type=int,choices=[0,1,2,3],default=1,help='Debug level: 0 is the lowest and 3 is the highest.')
+        parser.add_argument('--unroll',metavar="int",type=int,default=0,help='Used for Build mode: Number of times to unroll the interpreted variables.')
+        return  parser.parse_args()
 if __name__ == "__main__":
-    assert(len(argv)>=2)
-    if argv[1] =="Test":
+    args= parsing_CMD()
+    if args.procedure=="Test":
         Test.test()
-    elif argv[1] =="Build":
-        debug = int(argv[5]) if len(argv)==6 and argv[4] == "debug" else 0
-        Test.testExampleSize(argv[2],int(argv[3]),debug)
-    else:
-        debug = int(argv[3]) if len(argv)==4 and argv[2] == "debug" else 0
-        assert(4>=debug and debug>=0)
+    elif args.procedure=="Build":
+        Test.testExampleSize(args.unif_prob,args.unroll,args.debug)
+    elif args.procedure=="Amm":
         tp =TermParser()
         I = Interpretation()
-        with open(argv[1]) as f:
+        with open(args.unif_prob) as f:
             unif, mappings= tp.parse_input(f.readlines())
             for m in mappings:I.add_mapping(*m)
             I.add_relevent_vars(unif)
-            lu = LoopUnif(I,debug,unif)
+            lu = LoopUnif(I,args.debug,unif)
             lu.loop_unif()
--- Test.py
+++ Test.py
@@ -7,14 +7,14 @@ from os import listdir
 
 class Test:
     def test():
-        onlyfiles = [f for f in listdir("Examples") if "test" in f]
+        onlyfiles = [f for f in listdir("Examples/tests") if "test" in f]
         onlyfiles.sort(key= lambda a: int(a.split(".")[0][4:]) )
         isunif = {1:"unifiable",2:"unifiable",3:"unifiable",4:"not unifiable",5:"not unifiable",6:"not unifiable",7:"unifiable",8:"unifiable",9:"not unifiable",10:"not unifiable",11:"unifiable",12:"unifiable"}
         for i in range(1,len(onlyfiles)+1):
             print("Test "+str(i)+ " (should be "+isunif[i]+")")
             tp =TermParser()
             I = Interpretation()
-            with open("Examples/"+onlyfiles[i-1]) as f:
+            with open("Examples/tests/"+onlyfiles[i-1]) as f:
                 unif, mappings= tp.parse_input(f.readlines())
                 for m in mappings:I.add_mapping(*m)
                 lu = LoopUnif(I,0,unif)
